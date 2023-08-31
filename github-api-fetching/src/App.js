7e887900737fe05ce5d6ba84fb7be059f0bd0705
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2023-08-31 09:20:04 +0530

    Add files via upload

---
+++ github-api-fetching/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import GithubUser from './Component/GithubUser'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Github profiles</h1>
+      <GithubUser username="Samarth0606"/>
+      <GithubUser username="alok92iit"/>
+      <GithubUser username="SachinBarpanda"/>
+    </div>
+  )
+}
+
+export default App
